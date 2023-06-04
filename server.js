7d0be4175a7403f62e829fc7aab388a6650e4355
kopanbor <kopanbor@students.zhaw.ch>
2023-06-04 09:25:42 +0200

    Deploy Job

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello DevOps Course :-)');
+    res.send('Hello DevOps Course Deploy :-)');
 });
 app.listen(PORT, HOST);
 console.log(`Running on
